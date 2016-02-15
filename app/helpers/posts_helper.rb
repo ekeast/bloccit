f8e6bdb5d67adc338d2524fe197d6c3baf6a5ea7
Ted <ekeast328@gmail.com>
2016-02-15 12:09:53 -0500

    Checkpoint 32 Complete

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+
+  def render_title(post)
+    output = "<h1>#{post.filtered_title}</h1>".html_safe
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  def filtered_title
+    id % 5 == 0 || id == 0 ? "SPAM" : title
+  end
 end
