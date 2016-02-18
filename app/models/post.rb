237e7a528fcac0c8d1ea94d8ae433af15c829387
Ted <ekeast328@gmail.com>
2016-02-18 11:04:01 -0500

    Checkpoint 35 Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :topic
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+
   def filtered_title
     id % 5 == 0 || id == 0 ? "SPAM" : title
   end
