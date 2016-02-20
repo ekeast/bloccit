05381e8338912f9f5a3d1aafc21451853eab769c
Ted <ekeast328@gmail.com>
2016-02-19 20:32:42 -0500

    Checkpoint 38 Completed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe UsersController, type: :controller do
       expect(assigns(:user)).to_not be_nil
     end
   end
-  
+
   describe "POST create" do
     it "returns an http redirect" do
       post :create, user: new_user_attributes
@@ -51,6 +51,11 @@ RSpec.describe UsersController, type: :controller do
     it "sets user password_confirmation properly" do
       post :create, user: new_user_attributes
       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+    end
+
+    it "logs the user in after sign up" do
+      post :create, user: new_user_attributes
+      expect(session[:user_id]).to eq assigns(:user).id
     end
   end
 end
