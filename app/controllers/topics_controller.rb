c78eeb15fb8b9a4ae4704de72fd6000bf5673d72
Ted <ekeast328@gmail.com>
2016-02-23 10:32:46 -0500

    Checkpoint 41 COmplete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -43,6 +43,18 @@ class TopicsController < ApplicationController
     end
   end
 
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the topic."
+      render :show
+    end
+  end
+
   private
 
   def topic_params
@@ -53,19 +65,6 @@ class TopicsController < ApplicationController
     unless current_user.admin?
       flash[:alert] = "You must be an admin to do that."
       redirect_to topics_path
-    end
-  end
-
-
-  def destroy
-    @topic = Topic.find(params[:id])
-
-    if @topic.destroy
-      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-      redirect_to action: :index
-    else
-      flash.now[:alert] = "There was an error deleting the topic."
-      render :show
     end
   end
 end
