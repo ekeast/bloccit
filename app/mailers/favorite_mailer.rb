ea5d122afcf9ad06e1ba6bc0b55b4be96c6c445c
Ted <ekeast328@gmail.com>
2016-03-01 09:48:52 -0500

    Checkpoint 47 Completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,13 +12,4 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
-
-  def new_post(post)
-    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    headers["References"] = "<post/#{post.id}@your-app-name.example>"
-
-    @post = post
-    mail(to: post.user.email, subject: "#{post.title} has been favorited by you")
-  end
 end
